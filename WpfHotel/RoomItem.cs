790ebda0d7181fc90dac2b142a7a6a20e66875d0
xzjs <2310976219@qq.com>
2017-04-11 10:05:32 +0800

    天假预抵房取消功能

--- WpfHotel/RoomItem.cs
+++ WpfHotel/RoomItem.cs
@@ -74,6 +74,11 @@ namespace WpfHotel
                         contextMenu.Items.Add(menuItem5);
                         contextMenu.Items.Add(menuItem6);
                         break;
+                    case 2:
+                        var cancelMenuItem = new MenuItem { Header = "取消预约" };
+                        cancelMenuItem.Click += Cancel;
+                        contextMenu.Items.Add(cancelMenuItem);
+                        break;
                     case 3:
                         var renewMenuItem = new MenuItem { Header = "办理续住" };
                         renewMenuItem.Click += Renew;
@@ -94,6 +99,24 @@ namespace WpfHotel
                 }
                 return contextMenu;
             }
+        }
+
+        /// <summary>
+        /// 取消预约
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            using (var db=new hotelEntities())
+            {
+                Order order = db.Order.Where(o => o.RoomId == Room.Id).First(o => o.Finish == 0);
+                List<User> users = db.User.Where(u => u.OrderId == order.Id).ToList();
+                db.User.RemoveRange(users);
+                db.Order.Remove(order);
+                db.SaveChanges();
+            }
+            SetRoomStatus(1);
         }
 
         /// <summary>
@@ -213,9 +236,10 @@ namespace WpfHotel
                     var room = db.Room.Find(Room.Id);
                     room.Status = status;
                     db.SaveChanges();
-                    
+                    MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
+                    mainWindow.LoadRoomData();
                     //更新服务器
-                    
+
                     using (var client = new WebClient())
                     {
                         var response = client.UploadValues("http://" + config.Http + "/hotelClient/setRoomStatus.nd",
@@ -226,8 +250,7 @@ namespace WpfHotel
                         if ((string) jo["errorFlag"] != "false")
                             MessageBox.Show("设置房间状态失败");
                     }
-                    MainWindow mainWindow=Application.Current.MainWindow as MainWindow;
-                    mainWindow.LoadRoomData();
+                    
                 }
                 catch (WebException)
                 {
